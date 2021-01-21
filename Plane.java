0612d548753479b2a2c4219e78bd65555ac9f815
Sherkhan Khanseiit <57889104+Sherhan187@users.noreply.github.com>
2021-01-21 23:48:52 +0600

    Add files via upload

---
+++ Plane.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Plane {
+
+    private boolean landed;
+
+    public Plane() {
+        landed = true;
+    }
+
+    public boolean isLanded() {
+        return this.landed;
+    }
+
+    public void land() {
+        this.landed = true;
+    }
+
+    public void takeOff() {
+        this.landed = false;
+    }
+
+}
---
+++ Weather.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Random;
+
+public class Weather {
+
+    public boolean stormy() {
+        return randomNumber() <= 1;
+    }
+
+    private int randomNumber() {
+        Random rand = new Random();
+        return rand.nextInt(4) + 1;
+    }
+}
